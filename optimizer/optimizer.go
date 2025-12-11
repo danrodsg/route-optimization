2d2b964b30f95ed57c7981fd7404cfcfa8dc3c7b
Daniel Rodrigues <danielrods2004@gmail.com>
2025-12-11 14:12:01 -0300

    Optimizer.go

--- optimizer/optimizer.go
+++ optimizer/optimizer.go
@@ -1,1 +1,42 @@
 package optimizer
+
+import "math"
+
+type NearestNeighborOptimizer struct {
+	Calculator DistanceCalculator
+}
+
+func (n *NearestNeighborOptimizer) Optimize(startPoint Point, destinations []Point) []Point {
+	if len(destinations) == 0 {
+		return []Point{startPoint}
+	}
+
+	route := []Point{startPoint}
+
+	unvisited := make(map[string]Point)
+	for _, d := range destinations {
+		unvisited[d.ID] = d
+	}
+
+	currentPoint := startPoint
+
+	for len(unvisited) > 0 {
+		var nextPoint Point
+		minDistance := math.MaxFloat64
+
+		for _, nextCandidate := range unvisited {
+			dist := n.Calculator.Calculate(currentPoint, nextCandidate)
+
+			if dist < minDistance {
+				minDistance = dist
+				nextPoint = nextCandidate
+			}
+		}
+
+		route = append(route, nextPoint)
+		delete(unvisited, nextPoint.ID)
+		currentPoint = nextPoint
+	}
+
+	return route
+}
