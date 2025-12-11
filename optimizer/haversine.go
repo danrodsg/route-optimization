c5bacfe06d7f4c7699136026b442570575501c32
Daniel Rodrigues <danielrods2004@gmail.com>
2025-12-11 14:16:16 -0300

    Euclidean math

--- optimizer/haversine.go
+++ optimizer/haversine.go
@@ -1,1 +1,11 @@
 package optimizer
+
+import "math"
+
+type EuclideanDistance struct{}
+
+func (e EuclideanDistance) Calculate(p1, p2 Point) float64 {
+	latDiff := p1.Latitude - p2.Latitude
+	lonDiff := p1.Longitude - p2.Longitude
+	return math.Sqrt(latDiff*latDiff + lonDiff*lonDiff)
+}
