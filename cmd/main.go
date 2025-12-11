d0675b66b7addfa4170478bc9c2197f0399a8968
Daniel Rodrigues <danielrods2004@gmail.com>
2025-12-11 14:39:44 -0300

    Final Project

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-    "log"
-   
-    "github.com/danrodsg/route-optimization/api"
-    "github.com/gin-gonic/gin"
+	"fmt"
+
+	"github.com/danrodsg/route-optimization/optimizer"
 )
 
 func main() {
-    
-    router := gin.Default()
+	deposito := optimizer.Point{ID: "DEPOT", Latitude: -23.5505, Longitude: -46.6333} // São Paulo
 
-    router.POST("/api/v1/optimize", api.OptimizeRouteHandler)
+	campos := []optimizer.Point{
+		{ID: "CAMPO_A", Latitude: -23.54, Longitude: -46.65},
+		{ID: "CAMPO_B", Latitude: -23.60, Longitude: -46.70},
+		{ID: "CAMPO_C", Latitude: -23.50, Longitude: -46.60},
+		{ID: "CAMPO_D", Latitude: -23.58, Longitude: -46.68},
+	}
 
-    log.Println("Servidor de Otimização de Rotas rodando na porta 8080...")
-    if err := router.Run(":8080"); err != nil {
-        log.Fatalf("Erro ao iniciar o servidor: %v", err)
-    }
+	opt := optimizer.NearestNeighborOptimizer{
+		Calculator: optimizer.EuclideanDistance{},
+	}
+
+	rotaOtimizada := opt.Optimize(deposito, campos)
+
+	fmt.Println("--- Rota Otimizada ---")
+	for i, p := range rotaOtimizada {
+		fmt.Printf("%d. %s (Lat: %.4f, Lon: %.4f)\n", i+1, p.ID, p.Latitude, p.Longitude)
+	}
+
 }
